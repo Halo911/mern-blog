26e4aaf4eea8a0d18f9ab988b2e1a3d0e12c31cf
Harold Lam <halolam@gmail.com>
2024-02-27 17:17:23 -0500

    connect to the database

--- api/index.js
+++ api/index.js
@@ -1,6 +1,23 @@
 import expresss from 'express';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+mongoose
+  .connect(process.env.MONGO)
+  .then(() => {
+    console.log('MongoDB is connected');
+  })
+  .catch((err) => {
+    console.log(err);
+  });
 
 const app = expresss();
+
+//user: halolam
+//pwd: dmPRbn9lHqHHorls
+//mongodb+srv://halolam:<password>@mern-blog.ke3txuf.mongodb.net/?retryWrites=true&w=majority&appName=mern-blog
 
 app.listen(3000, () => {
   console.log('Server is running on port 3000!');
