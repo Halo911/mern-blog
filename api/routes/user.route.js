920a655892b3f46a5f5aff73fc66e5e711cd9d4e
Harold Lam <halolam@gmail.com>
2024-02-28 13:00:59 -0500

    create signup api route

--- api/routes/user.route.js
+++ api/routes/user.route.js
@@ -1,5 +1,13 @@
 import express from 'express';
 import { test } from '../controllers/user.controller.js';
+//import needs curly braces for named exports and doesn't need them for the default one
+/*
+CommonJS is the traditional module system in Node.js where modules are imported using require and exported using module.exports.
+ESM is the newer standard, which uses import and export statements, and is what's used in modern JavaScript development.
+When you add "type": "module" to your package.json, you are telling Node.js to treat .js files as ESM by default.
+Which could be an issue.
+When using ESM, Node.js expects the full file path including the extension. 
+*/
 
 const router = express.Router();
 
