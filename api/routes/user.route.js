f5649fb9d4d68d4df6ae166e3a810df3b7cb4127
Harold Lam <halolam@gmail.com>
2024-03-02 00:26:00 -0500

    add update user api route

--- api/routes/user.route.js
+++ api/routes/user.route.js
@@ -1,5 +1,6 @@
 import express from 'express';
-import { test } from '../controllers/user.controller.js';
+import { test, updateUser } from '../controllers/user.controller.js';
+import { verifyToken } from '../utils/verifyUser.js';
 //import needs curly braces for named exports and doesn't need them for the default one
 /*
 CommonJS is the traditional module system in Node.js where modules are imported using require and exported using module.exports.
@@ -12,5 +13,6 @@ When using ESM, Node.js expects the full file path including the extension.
 const router = express.Router();
 
 router.get('/test', test);
+router.put('/update/:userId', verifyToken, updateUser);
 
 export default router;
