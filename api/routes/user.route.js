fcf08eda39fb8154b5415b8eb3a1d9ec8dc7793f
Harold Lam <halolam@gmail.com>
2024-03-04 15:25:37 -0500

    create get users api route

--- api/routes/user.route.js
+++ api/routes/user.route.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import {
   deleteUser,
+  getUsers,
   signout,
   test,
   updateUser,
@@ -21,5 +22,6 @@ router.get('/test', test);
 router.put('/update/:userId', verifyToken, updateUser);
 router.delete('/delete/:userId', verifyToken, deleteUser);
 router.post('/signout', signout);
+router.get('/getusers', verifyToken, getUsers);
 
 export default router;
